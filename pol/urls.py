f40e9a99486e1625454ebdd9ba3dfb60acb04be1
simen <simennj@outlook.com>
2015-10-19 22:38:53 +0200

    rework url structure

--- pol/urls.py
+++ pol/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import url
+from django.views.generic.base import RedirectView
 
 from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<x>[0-9]+)/$', views.topX, name='topX'),
-    url(r'^liste$', views.liste),
+    url(r'^$', views.liste),
+    url(r'^liste/$', RedirectView.as_view(url='/pol/', permanent=False)),
+    # url(r'^(?P<x>[0-9]+)/$', views.topX, name='topX'),
     url(r'^table$', views.table),
     url(r'^produkt/(?P<pk>[0-9]+)/$', views.Produktvisning.as_view(), name='Produkt'),
     url(r'^produktside/(?P<pk>[0-9]+)/$', views.Produktside.as_view(), name='Produkt'),
-    url(r'^kart', views.map)
+    url(r'^kart', views.map),
 ]
--- polside/urls.py
+++ polside/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
-
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', RedirectView.as_view(url='pol/', permanent=False)),
     url(r'^pol/', include('pol.urls')),
 ]
