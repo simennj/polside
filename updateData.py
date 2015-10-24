cff1de8e92fe9f5ecb4b43962f3fc353150343cb
Simen <simennj@outlook.com>
2015-10-24 16:46:51 +0200

    fix dumb mistake

--- updateData.py
+++ updateData.py
@@ -1,10 +1,12 @@
+import urllib
 import csv
+import subprocess
 
-# produkterlaster = urllib.URLopener()
-# produkterlaster.retrieve("http://www.vinmonopolet.no/api/produkter", "produkter_raw.csv")
-# produkterlaster.close()
-#
-# subprocess.call("sed -i 's/,/./g' produkter_raw.csv")
+produkterlaster = urllib.URLopener()
+produkterlaster.retrieve("http://www.vinmonopolet.no/api/produkter", "produkter_raw.csv")
+produkterlaster.close()
+
+subprocess.call("sed -i 's/,/./g' produkter_raw.csv")
 
 with open("produkter_raw.csv", 'rb') as produkter:
     produkter_csv = csv.reader(produkter, delimiter=';')
