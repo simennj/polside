fc615f42f34f3b41be92cc301823aaa36ed72657
Simen <Simennj@outlook.com>
2015-12-29 18:00:11 +0100

    streamline updatedata process

--- data/updatedata.sh
+++ data/updatedata.sh
@@ -2,3 +2,5 @@
 python2.7 retrievedata.py
 sed -i 's/,/./g' produkter_raw.csv
 python2.7 formatdata.py
+mysql -u simennj -p"aldri_mer_PHP" polguide < deletedata.sql
+mysql -u simennj -p"aldri_mer_PHP" polguide < uploaddata.sql
