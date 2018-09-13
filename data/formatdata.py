dc69a43523e36add0d3764d3a7708ae512155323
Simen Norderud Jensen <simennj@outlook.com>
2018-09-13 12:14:53 +0200

    fix data update not working on column count change

--- data/formatdata.py
+++ data/formatdata.py
@@ -3,14 +3,15 @@ import csv
 with open("produkter_raw.csv", 'rb') as produkter:
     produkter_csv = csv.reader(produkter, delimiter=';')
     new_csv = []
-    produkter_csv.next()
+    header = produkter_csv.next()
+    columns = len(header)
     for row in produkter_csv:
-        if(len(row) == 36):
+        if(len(row) == columns):
             alk = float(row[26])
             row.append(float(row[5]) / alk if alk > 0 else 9000.01)
             new_csv.append(row)
         else:
-            print('row {0} has {1} elements, should have been 36'.format(row[1], len(row)))
+            print('row {0} has {1} elements, should have been {3}'.format(row[1], len(row), columns))
 
 JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 with open("produkter.csv", 'wb') as produkter:
